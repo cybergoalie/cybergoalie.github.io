088a140e562a3c8704760b41da0ee4871f12f250
CyberGoalie <99082706+cybergoalie@users.noreply.github.com>
2023-12-25 06:53:16 -0800

    Header, Intro & CTA

---
+++ script.js
@@ -0,0 +1,42 @@
+// script.js
+function toggleMenu() {
+    const navList = document.querySelector('nav ul');
+    navList.classList.toggle('active');
+    const menuToggle = document.querySelector('.menu-toggle');
+    menuToggle.classList.toggle('active');
+}
+
+function toggleTheme() {
+    const body = document.body;
+    body.classList.toggle('light-mode');
+    
+    const themeToggle = document.getElementById('theme-toggle');
+    const isLightMode = body.classList.contains('light-mode');
+    
+    // Set different icons based on the theme
+    if (isLightMode) {
+        themeToggle.innerHTML = '<i class="fa-solid fa-moon"></i>'; // Full moon for dark theme
+    } else {
+        themeToggle.innerHTML = '<i class="fas fa-sun" style="color: yellow; filter: drop-shadow(0 0 5px rgba(0, 0, 0, 0.5));"></i>';
+
+}
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const video = document.getElementById('backgroundVideo');
+    video.pause(); // Pause the video on page load
+});
+
+function toggleVideo() {
+    const video = document.getElementById('backgroundVideo');
+    const playButton = document.getElementById('playButton');
+
+    if (video.paused || video.ended) {
+        video.play();
+        playButton.innerHTML = '<i class="fas fa-pause"></i>';
+    } else {
+        video.pause();
+        playButton.innerHTML = '<i class="fas fa-play"></i>';
+    }
+}
+
