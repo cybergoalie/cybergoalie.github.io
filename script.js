485d971f859159f7a429f77ec57ba58d1f844ac3
CyberGoalie <99082706+cybergoalie@users.noreply.github.com>
2023-12-28 00:06:08 -0800

    Modal displays when imgs clicked upon and closes with X or click on overlay.

--- script.js
+++ script.js
@@ -68,13 +68,13 @@ document.addEventListener("DOMContentLoaded", function () {
                 certificate.style.zIndex = totalCertificates - adjustedIndex;
 
                 // Adjust the transform property to create the step-like appearance
-                const translateY = adjustedIndex * 35 + 8; // Increase this value to move the certificates further down
+                const translateY = adjustedIndex * 23 + -5; // Increase this value to move the certificates further down
 
                 // Calculate translateX dynamically based on screen width
-                const minTranslateX = (minTranslateXPercentage / 100) * screenWidth;
+                const minTranslateX = (minTranslateXPercentage / 100) * screenWidth
                 const maxTranslateX = (maxTranslateXPercentage / 100) * screenWidth;
                 const translateX =
-                    minTranslateX + ((maxTranslateX - minTranslateX) / screenWidth) * adjustedIndex * 35 + 8;
+                    minTranslateX + ((maxTranslateX - minTranslateX) / screenWidth) * adjustedIndex * 25 + 11;
 
                 certificate.style.transform = `translate(${translateX}px, ${translateY}px)`;
             } else {
@@ -93,6 +93,110 @@ document.addEventListener("DOMContentLoaded", function () {
         updateCertificates();
     });
 
+
+    // Adding keyboard event handling for right arrow
+    document.querySelector(".right").addEventListener("keydown", function (event) {
+        if (event.key === "Enter" || event.key === " ") {
+            currentIndex = (currentIndex + 1) % totalCertificates;
+            updateCertificates();
+            event.preventDefault();
+        }
+    });
+
+    // Adding keyboard event handling for left arrow
+    document.querySelector(".left").addEventListener("keydown", function (event) {
+        if (event.key === "Enter" || event.key === " ") {
+            currentIndex = (currentIndex - 1 + totalCertificates) % totalCertificates;
+            updateCertificates();
+            event.preventDefault();
+        }
+    });
+
     window.addEventListener("resize", updateCertificates); // Update certificates on window resize
     updateCertificates(); // Initial display
+});
+
+
+// LIGHTBOX MODAL
+
+document.addEventListener('DOMContentLoaded', function () {
+    const overlay = document.getElementById('overlay');
+
+    // Add a click event listener to the overlay
+    overlay.addEventListener('click', function () {
+        // Close the modal based on the lastModalId variable
+        closeModal(lastModalId);
+    });
+
+    // Click event to open the diploma modal
+    const diplomaImage = document.querySelector('.single-image img');
+    if (diplomaImage) {
+        diplomaImage.addEventListener('click', function (event) {
+            openDiplomaModal(event);
+            // Set the lastModalId variable for the overlay click event
+            lastModalId = 'diplomaModal';
+        });
+    }
+
+    // Click event to open the certificate modal
+    const certificateContainer = document.querySelector('.certificate-container');
+    if (certificateContainer) {
+        certificateContainer.addEventListener('click', function () {
+            openCertificatesModal();
+            // Set the lastModalId variable for the overlay click event
+            lastModalId = 'certificatesModal';
+        });
+    }
+
+    // Variable to store the last opened modal id
+    let lastModalId;
+
+    // Companion Function to close the modal
+    function closeModal(modalId) {
+        const modal = document.getElementById(modalId);
+        if (modal) {
+            console.log(`Attempting to close modal with ID: ${modalId}`);
+            modal.style.display = 'none';
+            console.log(`Closed modal with ID: ${modalId}`);
+            overlay.style.display = 'none';
+        } else {
+            console.log(`Modal with ID ${modalId} not found`);
+        }
+    }
+
+    // Function to open the diploma modal
+    function openDiplomaModal(event) {
+        const overlay = document.getElementById('overlay');
+        const modal = document.getElementById('diplomaModal');
+        const modalImage = document.getElementById('diplomaImage');
+    
+        // Access the clicked image's source from the event
+        const clickedImageSrc = event.target.src;
+        modalImage.src = clickedImageSrc; // Set the source dynamically
+        modal.style.display = 'block';
+        overlay.style.display = 'block';
+        console.log('Diploma modal opened. Display:', modal.style.display);
+    }
+    
+
+    // Function to open the certificates modal
+    function openCertificatesModal() {
+        const modal = document.getElementById('certificatesModal');
+        const overlay = document.getElementById('overlay');
+        const certificateSlider = document.querySelector('.certificate-slider');
+        const certificates = document.querySelectorAll('.certificate-container img');
+
+        // Clear existing content
+        certificateSlider.innerHTML = '';
+
+        // Clone and append each certificate image to the modal
+        certificates.forEach((certificate) => {
+            const clonedCertificate = certificate.cloneNode(true);
+            certificateSlider.appendChild(clonedCertificate);
+        });
+
+        modal.style.display = 'block';
+        overlay.style.display = 'block';
+        console.log('Certificates modal opened. Display:', modal.style.display);
+    }
 });
