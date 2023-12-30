ab2421cef7c071decf1e84ea2f5a0d271951ad70
CyberGoalie <99082706+cybergoalie@users.noreply.github.com>
2023-12-30 15:15:24 -0800

    Now always display the certificate it was closed on, whether navigated in modal or not.

--- script.js
+++ script.js
@@ -55,6 +55,8 @@ document.addEventListener("DOMContentLoaded", function () {
     const displayLimit = 3; // Set the initial display limit
     let currentIndex = 0;
 
+
+
     function updateCertificates() {
         const screenWidth = window.innerWidth;
         const maxTranslateXPercentage = -150; // Maximum translateX percentage for minimized screen
@@ -112,10 +114,99 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 
+    document.addEventListener('modalClosed', function (event) {
+        console.log('Received modalClosed event. Current Index of closed Certificate in the array:', event.detail.currentIndex);
+        currentIndex = event.detail.currentIndex;
+
+        console.log('Setting starting display of current index to the certificate it was closed on:', currentIndex);
+
+        updateCertificates();
+    });
+
+
     window.addEventListener("resize", updateCertificates); // Update certificates on window resize
     updateCertificates(); // Initial display
 });
 
+// Declare at the top level
+let currentCertificateIndex;
+let modalWasNavigated = false;
+
+function navigateCertificates(direction) {
+    const modal = document.getElementById('certificatesModal');
+    const certificateSlider = modal.querySelector('.certificate-slider');
+    const certificates = Array.from(document.querySelectorAll('.certificate-container img'));
+    const modalImage = certificateSlider.querySelector('.modal-content');
+
+    if (!modalImage) {
+        console.error('modalImage is undefined');
+        return;
+    }
+
+    console.log('Certificates array in navigateCertificates:', certificates);
+    console.log('Certificates length in navigateCertificates:', certificates.length);
+
+    if (certificates.length === 0) {
+        console.error('Certificates array is empty');
+        return;
+    }
+
+    let currentIndex = currentCertificateIndex;
+
+
+    if (direction === 'left') {
+        currentIndex = (currentIndex + 1 + certificates.length) % certificates.length;
+    } else if (direction === 'right') {
+        currentIndex = (currentIndex - 1 + certificates.length) % certificates.length;
+    }
+
+    const nextCertificate = certificates[currentIndex];
+
+    if (nextCertificate) {
+        modalImage.src = nextCertificate.src;
+        currentCertificateIndex = currentIndex;
+        modalWasNavigated = true; // Set the flag
+
+    } else {
+        console.error('Next certificate not found');
+    }
+}
+
+function closeModal(modalId) {
+    const modal = document.getElementById(modalId);
+    const overlay = document.getElementById('overlay');
+
+    if (modalId === 'diplomaModal') {
+
+    } else {
+
+        // Log the identified certificate that the modal was closed on
+        const closedCert = document.querySelector('.certificate-container img:not(.hidden)');
+        if (!closedCert) {
+            console.error('No certificate found. Aborting closeModal.');
+            return;
+        }
+        let currentIndex;
+
+        if (modalWasNavigated) {
+            currentIndex = currentCertificateIndex;
+            modalWasNavigated = false; // Reset the flag after using it
+
+        } else {
+
+            const certificates = document.querySelectorAll(".certificate-container img");
+            currentIndex = Array.from(certificates).indexOf(closedCert);
+        }
+        // Dispatch a custom event with the updated currentIndex
+        const closeModalEvent = new CustomEvent('modalClosed', { detail: { currentIndex } });
+        document.dispatchEvent(closeModalEvent);
+
+    }
+    modal.style.display = 'none';
+    overlay.style.display = 'none';
+    console.log(`Modal with ID ${modalId} closed. Display: ${modal.style.display}`);
+
+}
 
 // LIGHTBOX MODAL
 
@@ -157,7 +248,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
 });
 
-let currentCertificateIndex; // Declare a variable to store the current certificate index
 
 function openCertificatesModal(clickedCertificate) {
     const modal = document.getElementById('certificatesModal');
@@ -173,6 +263,10 @@ function openCertificatesModal(clickedCertificate) {
 
     // Find the index of the clicked certificate
     currentCertificateIndex = Array.from(certificates).findIndex(cert => cert === clickedCertificate);
+
+    console.log('Opened modal on certificate index:', currentCertificateIndex);
+    console.log('Opened modal on certificate source:', clickedCertificate.src);
+
 
     // Set the initially displayed certificate based on the clicked certificate
     modalImage.src = clickedCertificate.src;
@@ -207,82 +301,9 @@ if (certificateContainer) {
     });
 }
 
-function navigateCertificates(direction) {
-    const modal = document.getElementById('certificatesModal');
-    const certificateSlider = modal.querySelector('.certificate-slider');
-    const certificates = Array.from(document.querySelectorAll('.certificate-container img'));
-    const modalImage = certificateSlider.querySelector('.modal-content');
-
-    if (!modalImage) {
-        console.error('modalImage is undefined');
-        return;
-    }
-
-    console.log('Certificates:', certificates);
-    console.log('Certificates length:', certificates.length);
-
-    if (certificates.length === 0) {
-        console.error('Certificates array is empty');
-        return;
-    }
-
-    let currentIndex = currentCertificateIndex;
-
-    if (direction === 'left') {
-        currentIndex = (currentIndex + 1 + certificates.length) % certificates.length;
-    } else if (direction === 'right') {
-        currentIndex = (currentIndex - 1 + certificates.length) % certificates.length;
-    }
-
-    const nextCertificate = certificates[currentIndex];
-
-    if (nextCertificate) {
-        modalImage.src = nextCertificate.src;
-        currentCertificateIndex = currentIndex;
-    } else {
-        console.error('Next certificate not found');
-    }
-}
 
 
-function closeModal(modalId) {
-    const modal = document.getElementById(modalId);
-    const overlay = document.getElementById('overlay');
 
-    if (modalId === 'diplomaModal') {
-       
-    } else {
-
-        // Log the identified certificate that the modal was closed on
-        const currentCertificate = document.querySelector('.certificate-container img:not(.hidden)');
-        console.log('Closed on certificate:', currentCertificate);
-
-        if (!currentCertificate) {
-            console.error('No certificate found. Aborting closeModal.');
-            return;
-        }
-
-        const currentIndex = Array.from(document.querySelectorAll('.certificate-container img')).indexOf(currentCertificate);
-        console.log('Index of closed certificate:', currentIndex);
-
-        // Set the currentCertificate as the starting point in the loop of certificates
-        const certificates = document.querySelectorAll('.certificate-container img');
-
-        // Rearrange the certificates in the array by pushing the starting point to the front
-        for (let i = 0; i < currentIndex; i++) {
-            const temp = certificates[0].src;
-            for (let j = 0; j < certificates.length - 1; j++) {
-                certificates[j].src = certificates[j + 1].src;
-            }
-            certificates[certificates.length - 1].src = temp;
-        }
-        console.log('Number of certificates:', certificates.length);
-    }
-    modal.style.display = 'none';
-    overlay.style.display = 'none';
-    console.log(`Modal with ID ${modalId} closed. Display: ${modal.style.display}`);
-
-}
 
 
 
