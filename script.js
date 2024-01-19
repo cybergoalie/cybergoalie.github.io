9cf6df147cf0fb76da59003889331f978376a339
CyberGoalie <99082706+cybergoalie@users.noreply.github.com>
2024-01-18 18:21:28 -0800

    More fun with mobile styling.

--- script.js
+++ script.js
@@ -77,3 +77,16 @@ document.addEventListener('DOMContentLoaded', function () {
       circles[index].classList.add('active');
     }
   });
+
+  document.addEventListener("DOMContentLoaded", function () {
+    // Add smooth scrolling to all links
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+       anchor.addEventListener('click', function (e) {
+          e.preventDefault();
+
+          document.querySelector(this.getAttribute('href')).scrollIntoView({
+             behavior: 'smooth'
+          });
+       });
+    });
+ });
